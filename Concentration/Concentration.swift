3da002dbbb018243cf886b2d21de40d0e9ab35ed
JaeUng Hyun <jaeung@me.com>
2019-07-01 11:09:30 +0900

    indexOfOneAndOnlyFaceUpCard 메소드 변경

--- Concentration/Concentration.swift
+++ Concentration/Concentration.swift
@@ -10,17 +10,18 @@ struct Concentration {
     
     var indexOfOneAndOnlyFaceUpCard: Int? {
         get {
-            var foundIndex: Int?
-            for index in cards.indices {
-                if cards[index].isFaceUp {
-                    if foundIndex == nil {
-                        foundIndex = index
-                    } else {
-                        return nil
-                    }
-                }
-            }
-            return foundIndex
+            return cards.indices.filter { cards[$0].isFaceUp }.oneAndOnly
+//            var foundIndex: Int?
+//            for index in cards.indices {
+//                if cards[index].isFaceUp {
+//                    if foundIndex == nil {
+//                        foundIndex = index
+//                    } else {
+//                        return nil
+//                    }
+//                }
+//            }
+//            return foundIndex
         }
         set {
             for index in cards.indices {
@@ -67,4 +68,10 @@ struct Concentration {
         }
 	}
 	
+}
+
+extension Collection {
+    var oneAndOnly: Element? {
+        return count == 1 ? first : nil
+    }
 }
